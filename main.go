086f8e460cdb8f96c4925c12d814f7a6f051958a
Marcio Barbosa <marcio.barbosa@bemobi.com>
2025-01-24 15:11:48 -0300

    Added check ENV variable

--- main.go
+++ main.go
@@ -4,11 +4,21 @@ import (
 	"health-history/config"
 	"health-history/routes"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	if os.Getenv("ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Failed to load file .env")
+		}
+	}
+
 	r := gin.Default()
 	routes.SetupRoutes(r)
 	config.ConnectDatabase()
